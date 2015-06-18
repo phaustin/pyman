f2e3ff71497383b1705ee9aaaf7602108888fe0a
Phil Austin <paustin@eos.ubc.ca>
2015-06-18 14:40:58 -0700

    remove sphinxext, use official sphinx extentions

--- Book/conf.py
+++ Book/conf.py
@@ -16,8 +16,8 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('sphinxext'))
+#sys.path.insert(0, os.path.abspath('.'))
+#sys.path.append(os.path.abspath('sphinxext'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -28,14 +28,14 @@ sys.path.append(os.path.abspath('sphinxext'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 # extensions = ['sphinx.ext.mathjax']
           
-extensions = ['sphinx.ext.pngmath',
+extensions = ['sphinx.ext.mathjax',
               'matplotlib.sphinxext.only_directives',
               'matplotlib.sphinxext.plot_directive',
               'sphinx.ext.autodoc',
               'sphinx.ext.doctest',
-              'ipython_console_highlighting',
-              'numpydoc',
-              'plot2_directive']
+              'IPython.sphinxext.ipython_console_highlighting',
+              'numpydoc']
+
           
           
 # Add any paths that contain templates here, relative to this directory.
