9cf75f944bed8aa71beb1ca3e6d538417e38a509
Phil Austin <paustin@eos.ubc.ca>
2015-08-31 19:52:11 -0700

    working

--- Book/doit.sh
+++ Book/doit.sh
@@ -1,3 +1,2 @@
 #!/bin/bash -v
-/Users/phil/anaconda/bin/sphinx-build -b html -d _build/doctrees -E  /Users/phil/repos/dj_pine_text/Book _build/html
-# -d = cached directories   sourcedir outdir
+sphinx-build -N -b html . _build
